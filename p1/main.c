9123cccb05832203fe6a6cdf0ce050edadbb320f
braisf03 <114221451+braisf03@users.noreply.github.com>
2023-02-18 19:45:06 +0100

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,9 +1,3 @@
-/*
- * TITLE: PROGRAMMING II LABS       SUBTITLE: Practical1
- * AUTHOR 1: Brais Sánchez Ferreiro LOGIN 1: brais.sferreiro
- * AUTHOR 2: José Moreira Fariña    LOGIN 2: j.moreira1
- * GROUP: 3.3                       DATE: 25 / 03 / 22
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
