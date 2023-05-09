ce5118cb88557996ba8eef79478cea2c988294b0
braisf03 <114221451+braisf03@users.noreply.github.com>
2023-05-09 12:20:36 +0200

    Update bid_stack.h

--- p2/P2_IMPL_S/bid_stack.h
+++ p2/P2_IMPL_S/bid_stack.h
@@ -1,12 +1,3 @@
-/*
- * TITLE: PROGRAMMING II LABS
- * SUBTITLE: Practical 2
- * AUTHOR 1: Brais Sánchez Ferreiro        LOGIN 1: brais.sferreiro
- * AUTHOR 2: José Moreira Fariña           LOGIN 2: j.moreira1
- * GROUP: 3.3
- * DATE: 29 / 04 / 22
- */
-
 #ifndef BID_STACK_H
 #define BID_STACK_H
 
