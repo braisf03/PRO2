41366a638c70c21ff8b1f6b95aed24663cfcd13c
braisf03 <114221451+braisf03@users.noreply.github.com>
2023-05-09 12:22:00 +0200

    Update types.h

--- p2/P2_IMPL_S/types.h
+++ p2/P2_IMPL_S/types.h
@@ -1,12 +1,3 @@
-/*
- * TITLE: PROGRAMMING II LABS
- * SUBTITLE: Practical 2
- * AUTHOR 1: Brais Sánchez Ferreiro        LOGIN 1: brais.sferreiro
- * AUTHOR 2: José Moreira Fariña           LOGIN 2: j.moreira1
- * GROUP: 3.3
- * DATE: 29 / 04 / 22
- */
-
 #ifndef PRO2_2022_P2_TYPES_H
 #define PRO2_2022_P2_TYPES_H
 
