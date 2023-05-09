12141d60dcb50b5ee73d664ee4959032d9761c69
braisf03 <114221451+braisf03@users.noreply.github.com>
2023-05-09 12:20:27 +0200

    Update bid_stack.c

--- p2/P2_IMPL_S/bid_stack.c
+++ p2/P2_IMPL_S/bid_stack.c
@@ -1,12 +1,3 @@
-/*
- * TITLE: PROGRAMMING II LABS
- * SUBTITLE: Practical 2
- * AUTHOR 1: Brais Sánchez Ferreiro        LOGIN 1: brais.sferreiro
- * AUTHOR 2: José Moreira Fariña           LOGIN 2: j.moreira1
- * GROUP: 3.3
- * DATE: 29 / 04 / 22
- */
-
 #include "bid_stack.h"
 
 void createEmptyStack(tStack *stack){ //Crea una pila vacía
